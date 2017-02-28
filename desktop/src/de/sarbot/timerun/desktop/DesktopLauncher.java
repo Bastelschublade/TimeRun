cb42ccd336fcfda3d561aa308ef30ecd3af37922
sarbot <sarbot@finchen.localdomain>
2017-02-28 15:00:34 +0100

    new files

---
+++ desktop/src/de/sarbot/timerun/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package de.sarbot.timerun.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import de.sarbot.timerun.TimeRun;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new TimeRun(), config);
+	}
+}
